b1b038b7f4489fba3e17e73ee7c732a4cda5f1ce
猪肉降价 <162006117+zhuroujiangjia@users.noreply.github.com>
2024-03-17 12:43:00 +0800

    MainActivity.kt

---
+++ 第一次作业/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.zh2207020509no1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.zh2207020509no1.ui.theme.ZH2207020509No1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZH2207020509No1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ZH2207020509No1Theme{
+                        //GreetingText(message = "Happy Birthday Sam!",from = "From ZH")
+                        GreetingImage(message = "Happy Birthday Sam",from = "From ZH")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String,from: String, modifier: Modifier = Modifier){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ){
+
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+
+        )
+    }
+
+}
+
+
+@Composable
+fun GreetingImage(message: String,from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box (Modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+private fun GreetingPreview(message: String, from: String) {
+    ZH2207020509No1Theme {
+        //GreetingText(message = "Happy Birthday Sam!",from = "From ZH")
+        GreetingImage(message = "Happy Birthday Sam", from = "From ZH")
+    }
+}
+
